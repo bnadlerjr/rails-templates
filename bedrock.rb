f8f26bf8bb85f26b3935603a0ac5deff23b54642
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2009-11-09 01:47:01 -0500

    Set template root to github repo

--- bedrock.rb
+++ bedrock.rb
@@ -1,5 +1,5 @@
 # Base Rails template for setting up a new project.
-TEMPLATE_ROOT = "./rails-templates"
+TEMPLATE_ROOT = "http://github.com/thethirdswitch/rails-templates/raw/master"
 
 ['test-frameworks.rb', 'formtastic.rb', 'authlogic.rb', 'standard.rb', 'git.rb'].each do |template|
   load_template "#{TEMPLATE_ROOT}/#{template}"
