98a277e8830a9ec81236b469a1b326008d1a9043
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2009-11-09 22:35:29 -0500

    Added functional test for user session controller

--- authlogic.rb
+++ authlogic.rb
@@ -193,7 +193,9 @@ if yes?(ABOUT + "\ncontinue?(y/n)")
   CODE
 
   generate(:controller, "user_sessions")
-
+  route "map.resource :user_session, :only => [:new, :create, :destroy]"
+  route "map.root :controller => 'user_sessions', :action => 'new'"
+  
   file 'app/controllers/user_sessions_controller.rb', <<-CODE
   class UserSessionsController < ApplicationController
     before_filter :require_no_user, :only => [:new, :create]
@@ -221,8 +223,74 @@ if yes?(ABOUT + "\ncontinue?(y/n)")
   end
   CODE
 
-  route "map.resource :user_session, :only => [:new, :create, :destroy]"
+  file 'app/views/user_sessions/new.html.erb', <<-CODE
+  <p>Placeholder for new.html.erb</p>
+  CODE
+  
+  file 'test/functional/user_sessions_controller_test.rb', <<-CODE
+  require 'test_helper'
 
+  class UserSessionsControllerTest < ActionController::TestCase
+    setup do
+      @user = Factory.create(:user)
+      :activate_authlogic
+    end
+
+    context 'GET new' do
+      setup { get :new }
+
+      should_respond_with :success
+      should_render_template :new    
+    end
+
+    context 'POST create' do
+      context 'good credentials' do
+        setup { post :create, :user_session => { :email => "john.doe@example.com", :password => "secret" } }
+
+        should_assign_to :user_session
+        should_respond_with :redirect
+        should_redirect_to('root url') { root_url }
+        should_set_the_flash_to 'Login successful!'
+
+        should 'save user to session' do
+          assert(UserSession.find)
+        end      
+      end
+
+      context 'bad email' do
+        setup { post :create, :user_session => { :email => "bad@example.com", :password => "secret" } }
+
+        should_respond_with :success
+        should_render_template :new
+        should_set_the_flash_to 'Invalid email or password.'
+      end
+
+      context 'bad password' do
+        setup { post :create, :user_session => { :email => "john.doe@example.com", :password => "bad" } }
+
+        should_respond_with :success
+        should_render_template :new
+        should_set_the_flash_to 'Invalid email or password.'
+      end
+    end
+
+    context 'DELETE destroy' do
+      setup do
+        UserSession.create(@user)
+        delete :destroy
+      end
+
+      should_respond_with :redirect
+      should_redirect_to('new user session') { new_user_session_path }
+      should_set_the_flash_to 'Logout successful!'
+
+      should 'destroy the session' do
+        assert_nil(UserSession.find)
+      end
+    end
+  end
+  CODE
+  
   file 'app/controllers/application_controller.rb', <<-CODE
   # Filters added to this controller apply to all controllers in the application.
   # Likewise, all the methods added will be available for all controllers.
