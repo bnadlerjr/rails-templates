e2e615caa2a592232c85d09ab0ec63d114de65fd
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2009-11-11 18:31:00 -0500

    Open-uri seems to work; created a download method

--- authlogic.rb
+++ authlogic.rb
@@ -1,3 +1,12 @@
+require 'open-uri'
+
+def download(from, to = from.split("/").last)
+  file to, open(from).read
+rescue Exception => e
+  puts e.inspect
+  exit!
+end
+
 ABOUT = <<-CODE
 \n
 |---------------------------------------------------------------------------------|
@@ -22,6 +31,7 @@ ABOUT = <<-CODE
 CODE
 
 if yes?(ABOUT + "\ncontinue?(y/n)")
+  template_root = "http://github.com/thethirdswitch/rails-templates/raw/master/authlogic"
   gem "authlogic"
 
   generate(:session, "user_session")
@@ -29,40 +39,42 @@ if yes?(ABOUT + "\ncontinue?(y/n)")
 
   route "map.resources :users"
   
-  file "db/migrate/#{Time.now.utc.strftime("%Y%m%d%H%M%S")}_create_users.rb", <<-CODE
-class CreateUsers < ActiveRecord::Migration
- def self.up
-   create_table :users, :force => true do |t|
-     t.string :email,               :null => false
-     t.string :first_name,          :null => false
-     t.string :last_name,           :null => false
-     t.string :crypted_password,    :null => false
-     t.string :password_salt,       :null => false
-     t.string :persistence_token,   :null => false
-     t.string :single_access_token, :null => false
-     t.string :perishable_token,    :null => false
-     t.string :current_login_ip
-     t.string :last_login_ip
-
-     t.integer :login_count,        :null => false, :default => 0
-     t.integer :failed_login_count, :null => false, :default => 0
-
-     t.datetime :last_request_at
-     t.datetime :current_login_at
-     t.datetime :last_login_at
-   end
-
-   add_index :users, :email
-   add_index :users, :perishable_token
- end
-
- def self.down
-   remove_index :users, :perishable_token
-   remove_index :users, :email
-   drop_table :users
- end
-end
-  CODE
+  download "#{template_root}/create_users.rb", "db/migrate/#{Time.now.utc.strftime("%Y%m%d%H%M%S")}_create_users.rb"
+  
+#  file "db/migrate/#{Time.now.utc.strftime("%Y%m%d%H%M%S")}_create_users.rb", <<-CODE
+#class CreateUsers < ActiveRecord::Migration
+# def self.up
+#   create_table :users, :force => true do |t|
+#     t.string :email,               :null => false
+#     t.string :first_name,          :null => false
+#     t.string :last_name,           :null => false
+#     t.string :crypted_password,    :null => false
+#     t.string :password_salt,       :null => false
+#     t.string :persistence_token,   :null => false
+#     t.string :single_access_token, :null => false
+#     t.string :perishable_token,    :null => false
+#     t.string :current_login_ip
+#     t.string :last_login_ip
+#
+#     t.integer :login_count,        :null => false, :default => 0
+#     t.integer :failed_login_count, :null => false, :default => 0
+#
+#     t.datetime :last_request_at
+#     t.datetime :current_login_at
+#     t.datetime :last_login_at
+#   end
+#
+#   add_index :users, :email
+#   add_index :users, :perishable_token
+# end
+#
+# def self.down
+#   remove_index :users, :perishable_token
+#   remove_index :users, :email
+#   drop_table :users
+# end
+#end
+#  CODE
 
   file 'app/models/user.rb', <<-CODE
 class User < ActiveRecord::Base
