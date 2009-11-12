3d9b5cc0a8ae99b4f5d2577ce844c0baede789f6
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2009-11-11 20:32:28 -0500

    modified test-frameworks template to use external files

--- test-frameworks.rb
+++ test-frameworks.rb
@@ -1,10 +1,27 @@
+require 'open-uri'
+  
+def download(from, to = from.split("/").last)
+  file to, open("http://github.com/thethirdswitch/rails-templates/raw/master/test-frameworks/#{from}").read
+end
+
+def download_and_patch(from, to)
+  if File.exists?(to)
+    download(from, "#{to}.new")
+    run "diff -Naur #{to} #{to}.new > #{to}.diff"
+    run "patch -p0 < #{to}.diff"
+    run "rm #{to}.new #{to}.diff"
+  else
+    download(from, to)
+  end
+end
+
 ABOUT = <<-CODE
 \n
 |---------------------------------------------------------------------------------|
  Rails template for setting up testing frameworks.
 
- IMPORTANT: This template overwrites test_helper.rb and is meant to be run from a
-            fresh app.
+ IMPORTANT: This template will attempt to patch test_helper.rb. It will also 
+            remove the test/integration folder and any files in it.
 |---------------------------------------------------------------------------------|
 CODE
 
@@ -20,58 +37,5 @@ if yes?(ABOUT + "\ncontinue?(y/n)")
 
   generate :cucumber
 
-  file 'test/factories.rb', <<-CODE
-  # Example:
-  # Factory.define :user do |f|
-  #   f.email 'john.doe@example.com'
-  #   f.password 'secret'
-  #   f.password_confirmation 'secret'
-  #   f.first_name 'John'
-  #   f.last_name 'Doe'
-  # end
-  CODE
-
-  file 'test/test_helper.rb', <<-CODE
-  ENV["RAILS_ENV"] = "test"
-  ENV["AUTOFEATURE"] = "true"
-
-  require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-  require 'test_help'
-  require File.expand_path(File.dirname(__FILE__) + "/factories")
-
-  class ActiveSupport::TestCase
-    # Transactional fixtures accelerate your tests by wrapping each test method
-    # in a transaction that's rolled back on completion.  This ensures that the
-    # test database remains unchanged so your fixtures don't have to be reloaded
-    # between every test method.  Fewer database queries means faster tests.
-    #
-    # Read Mike Clark's excellent walkthrough at
-    #   http://clarkware.com/cgi/blosxom/2005/10/24#Rails10FastTesting
-    #
-    # Every Active Record database supports transactions except MyISAM tables
-    # in MySQL.  Turn off transactional fixtures in this case; however, if you
-    # don't care one way or the other, switching from MyISAM to InnoDB tables
-    # is recommended.
-    #
-    # The only drawback to using transactional fixtures is when you actually 
-    # need to test transactions.  Since your test is bracketed by a transaction,
-    # any transactions started in your code will be automatically rolled back.
-    self.use_transactional_fixtures = true
-
-    # Instantiated fixtures are slow, but give you @david where otherwise you
-    # would need people(:david).  If you don't want to migrate your existing
-    # test cases which use the @david style and don't mind the speed hit (each
-    # instantiated fixtures translates to a database query per test method),
-    # then set this back to true.
-    self.use_instantiated_fixtures  = false
-
-    # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-    #
-    # Note: You'll currently still have to declare fixtures explicitly in integration tests
-    # -- they do not yet inherit this setting
-    fixtures :all
-
-    # Add more helper methods to be used by all tests here...
-  end
-  CODE
+  download_and_patch 'test_helper.rb', 'test/test_helper.rb'
 end
