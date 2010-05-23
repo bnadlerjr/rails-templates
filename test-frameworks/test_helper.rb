7194a300b28f8ac2fff618a2343918aac465330c
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-05-23 18:25:33 -0400

    Normalized lines endings.

--- test-frameworks/test_helper.rb
+++ test-frameworks/test_helper.rb
@@ -32,10 +32,11 @@ class ActiveSupport::TestCase
   # then set this back to true.
   self.use_instantiated_fixtures  = false
 
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in 
+  # alphabetical order.
   #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
+  # Note: You'll currently still have to declare fixtures explicitly in 
+  # integration tests -- they do not yet inherit this setting
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
