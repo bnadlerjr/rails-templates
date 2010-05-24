ffdac1b53c7448ec7de1d88f934628ae03629531
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-05-24 02:04:41 -0400

    Cleaned up code formatting in bedrock template.

--- bedrock.rb
+++ bedrock.rb
@@ -1,10 +1,11 @@
 TEMPLATE_ROOT = "/Users/bob/Dev/rails-templates"
 
-# Base Rails template for setting up a new project.
-['test-frameworks.rb', 'formtastic.rb', 'authlogic.rb', 
- 'standard.rb', 'metric_fu.rb'].each do |template|
-  load_template File.join(TEMPLATE_ROOT, template)
-end
+TEMPLATES = [
+  'test-frameworks.rb', 'formtastic.rb', 'authlogic.rb', 'standard.rb', 
+  'metric_fu.rb'
+]
+
+TEMPLATES.each do |t| { load_template File.join(TEMPLATE_ROOT, t) }
 
 run "rake rails:freeze:gems"
 run "rake gems:install"
