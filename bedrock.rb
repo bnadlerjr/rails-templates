f1de302b9c7a82c81a2c0d7316cb29c1ae126d44
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-05-24 01:56:55 -0400

    Removed metric_fu folder; creating metric-fu rake task directly.

--- bedrock.rb
+++ bedrock.rb
@@ -2,7 +2,7 @@ TEMPLATE_ROOT = "/Users/bob/Dev/rails-templates"
 
 # Base Rails template for setting up a new project.
 ['test-frameworks.rb', 'formtastic.rb', 'authlogic.rb', 
- 'standard.rb'].each do |template|
+ 'standard.rb', 'metric_fu.rb'].each do |template|
   load_template File.join(TEMPLATE_ROOT, template)
 end
 
--- metric_fu.rb
+++ metric_fu.rb
@@ -1,24 +1,38 @@
-require 'open-uri'
-  
-def download(from, to = from.split("/").last)
-  file to, open("http://github.com/thethirdswitch/rails-templates/raw/master/metric_fu/#{from}").read
-end
-
-ABOUT = <<-CODE
+about = <<-CODE
 \n
-|---------------------------------------------------------------------------------|
- Rails template for setting up metric_fu.
-
-  * Creates / overwrites metric_fu.rake
-|---------------------------------------------------------------------------------|
+|-----------------------------------------------------------------------------|
+ Creates a custom rake task for metric-fu. Does not install metric-fu and its
+ dependencies in /vendor. If metric-fu is not installed the this task will not
+ be available.
+|-----------------------------------------------------------------------------|
 CODE
 
-if yes?(ABOUT + "\ncontinue?(y/n)")
-  gem 'relevance-rcov', :environment => :test, :lib => 'rcov'
-  gem 'ruby-prof', :environment => :test
-  gem 'jscruggs-metric_fu', :environment => :test, :lib => 'metric_fu'
-  gem 'reek', :environment => :test
-  gem 'roodi', :environment => :test
+if yes?(about + "\ncontinue?(y/n)")
+  file "lib/tasks/metric_fu.rake", <<-CODE
+begin
+  require 'metric_fu'
 
-  download "metric_fu.rake", "lib/tasks/metric_fu.rake"
+  MetricFu::Configuration.run do |config|
+    config.metrics = [:churn, :stats, :flog, :flay, :reek, :roodi, :rcov]
+  
+    # Uncomment next line if ImageMagick, RMagick & Gruff are not installed
+    #config.graphs  = []
+  
+    config.flay    = { :dirs_to_flay  => ['app', 'lib'] }
+    config.flog    = { :dirs_to_flog  => ['app', 'lib'] }
+    config.reek    = { :dirs_to_reek  => ['app', 'lib'] }
+    config.roodi   = { :dirs_to_roodi => ['app', 'lib'] }
+    config.churn   = { :start_date    => '1 year ago', :minimum_churn_count => 10}
+    config.rcov    = { :test_files    => ['test/**/*_test.rb'],
+                       :rcov_opts     => ['--sort coverage', '--no-html', 
+                                          '--text-coverage', '--no-color', 
+                                          '--profile',       '--rails', 
+                                          '--exclude /gems/,/Library/,spec', 
+                                          '-Itest'
+                       ]
+                     }
+  end
+rescue LoadError
+end
+CODE
 end
