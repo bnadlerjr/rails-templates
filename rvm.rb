2373763c6da7fd347da3f3a4fe928d6938f0c12b
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-12-23 10:15:47 -0500

    * jQuery template to replace Prototype. * Remove rvmrc template and use echo instead.

--- rvm.rb
+++ rvm.rb
@@ -3,5 +3,5 @@ path = File.join(File.dirname(__FILE__), "templates")
 puts " Adding an .rvmrc file (ruby 1.9.2) "
 puts "-------------------------------------------------------------------------"
 run  "touch .rvmrc"
-run  "cat #{path}/rvmrc >> .rvmrc"
+run  "echo 'rvm 1.9.2' >> .rvmrc"
 puts "-------------------------------------------------------------------------"
