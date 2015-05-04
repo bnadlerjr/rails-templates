9bf99ab290dd60e7776e3b0b7b28130a3d368117
Bob Nadler <bnadlerjr@gmail.com>
2015-05-04 17:40:38 -0400

    Remove old application templates.

--- snippets/devise.rb
+++
@@ -1,9 +0,0 @@
-@commands.pre_bundle do
-  puts " Configuring Devise"
-  puts "-------------------------------------------------------------------------"
-  gem  'devise'
-end
-
-@commands.post_bundle do
-  generate 'devise:install'
-end
--- snippets/metric_fu.rb
+++
@@ -1,6 +0,0 @@
-@commands.pre_bundle do
-  puts " Configuring metric_fu"
-  puts "-------------------------------------------------------------------------"
-
-  gem  'metric_fu'
-end
