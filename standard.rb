642cba459eb531d6e26822ab99cdbea193d5ea79
Bob Nadler <bnadlerjr@gmail.com>
2018-12-13 22:54:29 -0500

    Gemfile template

--- standard.rb
+++ standard.rb
@@ -1,10 +1,13 @@
-require_relative "lib/commands"
+# require_relative "lib/commands"
 
-@commands = Commands.new
+# @commands = Commands.new
 
-%w(rspec cucumber rspec_cucumber_ci letter_opener bullet heroku devise).each do |cmd|
-  apply File.join(File.dirname(__FILE__), "commands", "#{cmd}.rb")
-end
+# %w(rspec cucumber rspec_cucumber_ci letter_opener bullet heroku devise).each do |cmd|
+  # apply File.join(File.dirname(__FILE__), "commands", "#{cmd}.rb")
+# end
 
-@commands.execute(:pre_bundle)
-after_bundle { @commands.execute(:post_bundle) }
+# @commands.execute(:pre_bundle)
+# after_bundle { @commands.execute(:post_bundle) }
+
+source_paths.unshift(File.join(File.dirname(__FILE__), 'lib', 'templates'))
+template 'Gemfile.tt', force: true
