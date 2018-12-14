18dc285f751814bc48fba6a58194ce59cddf3304
Bob Nadler <bnadlerjr@gmail.com>
2018-12-13 22:03:37 -0500

    WIP: heroku support

--- standard.rb
+++ standard.rb
@@ -2,7 +2,7 @@ require_relative "lib/commands"
 
 @commands = Commands.new
 
-%w(rspec cucumber rspec_cucumber_ci letter_opener bullet heroku devise gimlet).each do |cmd|
+%w(rspec cucumber rspec_cucumber_ci letter_opener bullet heroku devise).each do |cmd|
   apply File.join(File.dirname(__FILE__), "commands", "#{cmd}.rb")
 end
 
